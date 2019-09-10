572f550f5fbe9620c739da9d2bca3d2f4c30bbbf
James Calnan <James.Calnan@outlook.com>
2019-09-10 19:53:33 +0100

    Maze scales correctly

--- Input Maze/Input Maze/Module1.vb
+++ Input Maze/Input Maze/Module1.vb
@@ -4,23 +4,44 @@ Imports System.IO
 Module Module1
 
     Sub Main()
+        Console.CursorVisible = False
         Dim list As New List(Of Cell)
-        Dim imageLocation As String = "new maze 2 m 1.png"
+        Dim multiplier As Integer = 8
+        Dim imageLocation As String = "reading test 4 m 8.png"
         Console.ReadKey()
+        Dim PathOnMaze As Boolean = False
         Dim image As New Bitmap(imageLocation)
-        For x = 1 To (image.Width - 1) Step 1
-            For y = 1 To image.Height - 1 Step 1
+        For y = 1 To image.Height Step multiplier * 2
+            For x = 1 To image.Width Step multiplier * 2
                 Dim pixel As Color = image.GetPixel(x, y)
-                If pixel.GetBrightness <> 0 Then
-                    list.Add(New Cell(x * 2, y))
+                If pixel.GetBrightness = 1 Then
+                    Dim b As Integer = pixel.GetBrightness
+                    list.Add(New Cell(x / multiplier, y / (multiplier * 2)))
+                    Console.BackgroundColor = ConsoleColor.White
+                    Console.ForegroundColor = ConsoleColor.White
+                    Console.SetCursorPosition(x / multiplier, y / (multiplier * 2))
+                    Console.Write("XX")
+                End If
+                If pixel.GetBrightness <> 0 And pixel.GetBrightness <> 1 Then
+                    PathOnMaze = True
+                    Dim c As ConsoleColor = ConsoleColor.Green
+                    Console.BackgroundColor = c
+                    Console.ForegroundColor = c
+                    Console.SetCursorPosition(x / multiplier, y / (multiplier * 2))
+                    Console.Write("XX")
                 End If
             Next
         Next
         Console.BackgroundColor = ConsoleColor.White
         Console.ForegroundColor = ConsoleColor.White
-        For Each node In list
-            node.Print("██")
-        Next
+        If Not PathOnMaze Then
+            Console.BackgroundColor = ConsoleColor.Red
+            Console.ForegroundColor = ConsoleColor.Red
+            list(0).Print("XX")
+            Console.BackgroundColor = ConsoleColor.Green
+            Console.ForegroundColor = ConsoleColor.Green
+            list(list.Count - 1).Print("XX")
+        End If
         Console.ReadKey()
     End Sub
     Function Brightnes(ByVal c As Color) As Double
